59b6e6e4b3e2b4fc19d6cf8f6f4e81c39ea5d4a4
John Patrick Cube <akosiccc@gmail.com>
2024-03-28 16:51:46 +0800

    Movement and Animation Update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,12 +8,15 @@ public class PlayerController : MonoBehaviour
 {
     CharacterController character_obj;
     Vector3 cam_rotation;
-    public float moveSpeed = 1f; 
+    public float moveSpeed = 1f;
+    public float walkScale = 0.3f;
+    public float runScale = 1f;
     public float rotationSpeed = 10f; // Adjust the rotation speed as needed
     public Camera cam_obj;
     private PlayerInput playerInput;
     private Vector2 moveInput;
     private bool isJumping;
+    private Animator animator;
     
     // Start is called before the first frame update
     void Awake()
@@ -25,6 +28,8 @@ public class PlayerController : MonoBehaviour
         character_obj = GetComponent<CharacterController>();
 
         playerInput = GetComponent<PlayerInput>();
+
+        animator = GetComponent<Animator>();
 
         playerInput.actions["Horizontal"].performed += OnHorizontalPerformed;
         playerInput.actions["Horizontal"].canceled += OnHorizontalCanceled;
@@ -69,46 +74,24 @@ public class PlayerController : MonoBehaviour
         camForward.y = 0f;
         camRight.y = 0f;
         Vector3 moveDirection = (camForward.normalized * verticalInput + camRight.normalized * horizontalInput).normalized;
-
         if (moveDirection != Vector3.zero)
         {
             Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
-        character_obj.Move(moveDirection * moveSpeed * Time.deltaTime);
-
-        // Get input axes relative to camera
-        /*moveInput = new Vector3(moveInput.x * moveSpeed, 0f, moveInput.y * moveSpeed);
-        Vector3 moveDirection = Vector3.Normalize(Vector3.ProjectOnPlane(cam_obj.transform.forward, Vector3.up)) * moveInput.z + Vector3.Normalize(Vector3.ProjectOnPlane(cam_obj.transform.right, Vector3.up)) * moveInput.x;
-
-        // Rotate towards movement direction (if there's any input)
-        if (moveDirection.magnitude > 0)
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        float magnitudeTotal = moveInput.magnitude > 0f ? (moveInput.magnitude > 0.5f ? runScale : walkScale ) : 0f;
+        if (magnitudeTotal == runScale){
+            animator.SetBool("Idle", false); animator.SetBool("Walk", false); animator.SetBool("Run", true);
         }
-
-        Debug.Log(moveDirection.normalized.x);*/
-        // Smoothly move the character
-        //Vector3 movement = new Vector3(moveDirection.normalized.x * moveSpeed * Time.deltaTime, 0f, moveDirection.normalized.z * moveSpeed * Time.deltaTime);
-        //character_obj.Move(movement);
-
-        /*moveSpeed = new Vector3(movespeed_multiplier * moveInput.x, 0, movespeed_multiplier * moveInput.y);
-        cam_rotation = cam_obj.transform.TransformDirection(moveSpeed); cam_rotation.Set(cam_rotation.x, 0, cam_rotation.z);
-
-        if (character_obj.isGrounded) { 
-            moveapply = cam_rotation.normalized * moveSpeed.magnitude; moveapply.Set(moveapply.x, 0, moveapply.z); 
+        else if (magnitudeTotal == walkScale){
+            animator.SetBool("Idle", false); animator.SetBool("Walk", true); animator.SetBool("Run", false);
         }
-        else {
-            //moveapply = cam_rotation.normalized * moveSpeed.magnitude; moveapply.Set(moveapply.x, -9.81f * Time.deltaTime, moveapply.z);
-            moveapply = cam_rotation.normalized * moveSpeed.magnitude; moveapply.Set(moveapply.x, 0, moveapply.z);
+        else{
+            animator.SetBool("Idle", true); animator.SetBool("Walk", false); animator.SetBool("Run", false);
         }
-        if (cam_rotation.normalized != Vector3.zero){
-            Quaternion targetRotation = Quaternion.LookRotation(cam_rotation.normalized);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-        }
-        character_obj.Move(moveapply);*/
-        //character_obj.Move(moveSpeed);
+        Vector3 moveResultant = moveDirection * moveSpeed * magnitudeTotal * Time.deltaTime;
+        
+        character_obj.Move(moveResultant);
     }
     private void OnDestroy()
     {
